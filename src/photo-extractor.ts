9fd1c726fd7c8cba64a07469aac28147ad5ad287
kristeehan <kristeehan@gmail.com>
2023-10-21 10:15:57 -0400

    getting dimensions

--- src/photo-extractor.ts
+++ src/photo-extractor.ts
@@ -4,11 +4,19 @@ interface photoMetadata {
   date?: string;
 }
 
+interface imageDimensions {
+  width: number;
+  height: number;
+}
+
 export function extractMetaData(image: File): photoMetadata {
   const reader = new FileReader();
   reader.readAsDataURL(image);
   reader.onload = function () {
     console.log(reader.result);
+    getImageSize(reader.result as string).then((dimensions) => {
+      console.log(dimensions);
+    });
   };
 
   return {
@@ -21,3 +29,26 @@ export function extractMetaData(image: File): photoMetadata {
 // function getImageMetaData(fileResult) {
 //   console.log(fileResult);
 // }
+
+/**
+ * Get the image size from the passed file reader result
+ * Create a temporary image element, return the width/height as a json object
+ * @param fileResult
+ */
+function getImageSize(image: string): Promise<imageDimensions> {
+  const $image = document.createElement("img");
+  const dimensionsPromise = new Promise<imageDimensions>((resolve) => {
+    $image.onload = function () {
+      resolve({
+        width: $image.width,
+        height: $image.height,
+      });
+    };
+  });
+  $image.src = image;
+  $image.onload = function () {
+    console.log($image.width);
+    console.log($image.height);
+  };
+  return dimensionsPromise;
+}
