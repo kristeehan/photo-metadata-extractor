25b81ba82f2a8b1e57d8b8dded343c708b24285f
kristeehan <kristeehan@gmail.com>
2023-11-03 13:48:36 -0400

    test cleanup

--- src/__tests__/MetaDataCard.test.tsx
+++ src/__tests__/MetaDataCard.test.tsx
@@ -6,15 +6,12 @@ import MetaDataCard from "../MetaDataCard";
 
 const imagePath = path.join(__dirname, "assets", "test-image.jpg");
 const imageBuffer = fs.readFileSync(imagePath);
-// Create a Blob from the binary data
 const blob = new Blob([imageBuffer], { type: "image/jpeg" });
 const file = new File([blob], "test-image.jpg", { type: "image/jpeg" });
 
 describe("MetaDataCard", () => {
   it("Renders", () => {
-    // Fetch the mock JPEG file from the public folder
     const card = render(<MetaDataCard imageFile={file} />);
-    // verify rendered component
     expect(card.getByTestId("metadata-card")).toBeTruthy();
     card.unmount();
   });
@@ -28,10 +25,13 @@ describe("MetaDataCard", () => {
 
   it("Renders the metadata list on mouse hover", async () => {
     const card = render(<MetaDataCard imageFile={file} />);
-    const hoverIcon = await screen.findByTestId("hover-icon");
+    const hoverIcon = await screen.findByTestId("hover-icon-svg");
     fireEvent.mouseEnter(hoverIcon);
     const metadataList = await screen.findByTestId("metadata-list");
     expect(metadataList).toBeTruthy();
+    expect(
+      metadataList.classList.contains("metadata-list--display"),
+    ).toBeTruthy();
     card.unmount();
   });
 
@@ -41,7 +41,6 @@ describe("MetaDataCard", () => {
     fireEvent.mouseEnter(hoverIcon);
     const metadataList = await screen.findByTestId("metadata-list");
     expect(metadataList).toBeTruthy();
-    console.log(metadataList.classList.toString(), " our class list");
     expect(
       metadataList.classList.contains("metadata-list--display"),
     ).toBeTruthy();
@@ -51,28 +50,4 @@ describe("MetaDataCard", () => {
     ).toBeFalsy();
     card.unmount();
   });
-
-  // it("displays metadata on mouse hover", async () => {
-  //   const imageFile = new File([], "test.jpg", { type: "image/jpeg" });
-  //   render(<MetaDataCard imageFile={imageFile} />);
-  //   const iconOverlay = screen.getByTestId("icon-overlay");
-  //   fireEvent.hover(iconOverlay);
-  //   const metadataList = await screen.findByTestId("metadata-list");
-  //   expect(metadataList).toBeInTheDocument();
-  //   Object.entries(mockMetadata).forEach(([key, value]) => {
-  //     expect(screen.getByText(keyToLabelMap[key])).toBeInTheDocument();
-  //     expect(screen.getByText(value)).toBeInTheDocument();
-  //   });
-  // });
-
-  // it("hides metadata on mouse leave", async () => {
-  //   const imageFile = new File([], "test.jpg", { type: "image/jpeg" });
-  //   render(<MetaDataCard imageFile={imageFile} />);
-  //   const iconOverlay = screen.getByTestId("icon-overlay");
-  //   fireEvent.hover(iconOverlay);
-  //   const metadataList = await screen.findByTestId("metadata-list");
-  //   expect(metadataList).toBeInTheDocument();
-  //   fireEvent.unhover(metadataList);
-  //   expect(metadataList).not.toBeInTheDocument();
-  // });
 });
